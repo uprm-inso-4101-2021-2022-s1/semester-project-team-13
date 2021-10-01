2558ad092878eadea2858088b7f94340e46a3dec
JeremyCaban <jeremy.caban@upr.edu>
2021-10-01 13:12:08 -0400

    Pass beaches to front=end, Display limited comment

--- beach/views.py
+++ beach/views.py
@@ -1,12 +1,14 @@
+from os import name
 from django.shortcuts import render
-from .models import Comment
+from .models import Comment, Beach
 # Views are like request handlers.
 # Can map URL's to views.
 # Create your views here.
-
-def display(request, beachName='jobos'):
+commentAmount = 3
+def display(request, beachName='Jobos'):
     context = {
-        'comments': Comment.objects.filter(beach__name=beachName),
-        #'comments': getComment(),
+        'comments': Comment.objects.filter(beach__name=beachName)[:commentAmount], #front-end needs button that modifies the commentAmount variable
+        'beaches': Beach.objects.filter(name=beachName),
+        #'comments': Comment.objects.all()[:69],
     }
     return render(request, 'beach/display.html', context)
